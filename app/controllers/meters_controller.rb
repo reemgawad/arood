7d8b502d6280a2715773998ef388ea173a6d4424
Reem Gawad <reemabdelg@gmail.com>
2022-09-01 17:29:37 -0400

    added logic for character translation

--- app/controllers/meters_controller.rb
+++ app/controllers/meters_controller.rb
@@ -2,4 +2,29 @@ class MetersController < ApplicationController
   def index
     @meters = Meter.all
   end
+
+  def translate(character)
+    tashkil = [' ُ', ' َ', ' ِ']
+    tanween = [' ٌ', ' ً', ' ٍ']
+    sokoon = ' ْ'
+    shadda = ' ّ'
+    madd = ' ٓ'
+
+    binary = case character
+             when *tashkil
+               '1'
+             when *tanween
+               '0'
+             when sokoon
+               '0'
+             when shadda
+               '01'
+             when madd
+               '10'
+             else
+               '0'
+             end
+    binary
+  end
+
 end

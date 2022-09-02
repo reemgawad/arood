28d4077d2110cb07fea59e4b9e395fcc2e2cdd8a
Reem Gawad <reemabdelg@gmail.com>
2022-09-01 21:36:57 -0400

    adding input field

--- app/controllers/meters_controller.rb
+++ app/controllers/meters_controller.rb
@@ -10,21 +10,19 @@ class MetersController < ApplicationController
     shadda = ' ّ'
     madd = ' ٓ'
 
-    binary = case character
-             when *tashkil
-               '1'
-             when *tanween
-               '0'
-             when sokoon
-               '0'
-             when shadda
-               '01'
-             when madd
-               '10'
-             else
-               '0'
-             end
-    binary
+    case character
+    when *tashkil
+      '1'
+    when *tanween
+      '0'
+    when sokoon
+      '0'
+    when shadda
+      '01'
+    when madd
+      '10'
+    else
+      '0'
+    end
   end
-
 end
